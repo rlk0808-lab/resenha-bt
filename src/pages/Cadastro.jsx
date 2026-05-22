9936a48e309139b74c282d972e0bc64dc6d1deef
Robson <rlk0808@gmail.com>
2026-05-22 10:16:11 -0300

    fix: adicionar export default no Cadastro.jsx

---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,14 @@
+export default function Cadastro() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#0a1628',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#fff'
+    }}>
+      <h1>Cadastro</h1>
+    </div>
+  )
+}
